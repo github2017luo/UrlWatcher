e78ee0f8cc935cb761341e9865027a5022281c1d
xyx0826 <xyx0826@hotmail.com>
2019-02-28 15:53:14 -0500

    添加项目文件。

---
+++ UrlWatcher/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace UrlWatcher
+{
+    class Program
+    {
+        private const int RefreshIntervalMs = 5000;
+
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                PrintInfo("Checking...");
+                Thread.Sleep(RefreshIntervalMs);
+                RssChecker.Refresh();
+            }
+        }
+
+        internal static void PrintInfo(string message)
+        {
+            Console.WriteLine($"{DateTime.Now.ToLongTimeString()}: {message}");
+        }
+
+        internal static void PrintError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{DateTime.Now.ToLongTimeString()}: {message}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        internal static void PrintSuccess(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{DateTime.Now.ToLongTimeString()}: {message}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        [DllImport("user32.dll", EntryPoint = "FlashWindow")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool _flashWindow(
+            IntPtr hWnd,
+            [MarshalAs(UnmanagedType.Bool)]
+            bool bInvert);
+
+        internal static void FlashWindow()
+        {
+            var handle = Process.GetCurrentProcess().MainWindowHandle;
+            _flashWindow(handle, false);
+        }
+    }
+}
---
+++ UrlWatcher/RssChecker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Media;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace UrlWatcher
+{
+    internal static class RssChecker
+    {
+        private static DateTimeOffset _lastOffset;
+        private static XmlReader reader;
+
+        internal static void Refresh()
+        {
+            reader = XmlReader.Create("https://osu.ppy.sh/feed/ranked/");
+            var feed = SyndicationFeed.Load(reader);
+            if (feed.Items.Count() == 0)
+            {
+                Program.PrintError("SyndicationFeed item count is zero.");
+                return;
+            }
+
+            var latestItem = feed.Items.First();
+            var latestOffset = latestItem.PublishDate;
+            if (_lastOffset != DateTimeOffset.MinValue
+                && !_lastOffset.Equals(latestOffset))
+            {
+                var link = latestItem.Links.First().Uri.AbsoluteUri;
+                Program.PrintSuccess("New feed found.");
+                Program.FlashWindow();
+                Process.Start(link);
+                PlayAudio();
+            }
+            _lastOffset = latestOffset;
+        }
+
+        internal static void PlayAudio()
+        {
+            var stream = Properties.Resources.ApexVictory;
+            var player = new SoundPlayer(stream);
+            player.Play();
+        }
+    }
+}
